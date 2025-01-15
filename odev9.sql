73b3259b00bec5c08b394fa26f3444384b3d22ee
busrahadimlioglu <busrahadimlioglu@gmail.com>
2025-01-15 15:54:09 +0300

    add odev 9

---
+++ odev9.sql
@@ -0,0 +1,12 @@
+--9.Ödev
+SELECT city, country FROM country 
+INNER JOIN city 
+ON country.country_id = city.country_id;
+
+SELECT first_name, last_name, payment_id FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id;
+
+SELECT first_name, last_name, rental_id FROM customer
+INNER JOIN rental
+ON customer.customer_id = rental.customer_id;
