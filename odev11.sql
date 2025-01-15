e45cb241772d2028cb3f65e95fa0582719370034
busrahadimlioglu <busrahadimlioglu@gmail.com>
2025-01-15 16:34:10 +0300

    add odev 11

---
+++ odev11.sql
@@ -0,0 +1,42 @@
+--11.Ödev
+(
+SELECT first_name FROM actor
+)
+UNION
+(
+SELECT first_name FROM customer
+);
+
+(
+SELECT first_name FROM actor
+)
+INTERSECT
+(
+SELECT first_name FROM customer
+);
+
+(
+SELECT first_name FROM actor
+)
+EXCEPT
+(
+SELECT first_name FROM customer
+);
+
+--Tekrar eden verilerle
+(
+SELECT first_name FROM actor
+)
+UNION ALL
+(
+SELECT first_name FROM customer
+);
+
+SELECT actor.first_name FROM actor
+INNER JOIN customer
+ON actor.first_name = customer.first_name;
+
+SELECT actor.first_name FROM actor
+LEFT JOIN customer
+ON actor.first_name = customer.first_name
+WHERE customer.first_name IS NULL;
