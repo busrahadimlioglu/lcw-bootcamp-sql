7e1f51907a9d457047deac58b74d4a681cc1644f
busrahadimlioglu <busrahadimlioglu@gmail.com>
2025-01-15 13:19:15 +0300

    add odev 6

---
+++ odev6.sql
@@ -0,0 +1,6 @@
+-- 6.Ödev
+
+SELECT AVG(rental_rate) FROM film; --2.9800000000000000
+SELECT COUNT(*) FROM film WHERE title LIKE 'C%'; --92
+SELECT MAX(length) FROM film WHERE rental_rate = 0.99; --184
+SELECT COUNT(DISTINCT(replacement_cost)) FROM film WHERE length > 150; --21
